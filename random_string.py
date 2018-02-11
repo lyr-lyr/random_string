70137a976a15c8e568674bb7f07ce5799bf6b520
lyr-lyr <36128658+lyr-lyr@users.noreply.github.com>
2018-02-12 00:10:36 +0800

    Update random_string.py

--- random_string.py
+++ random_string.py
@@ -5,19 +5,11 @@ import random
 str_digits = '0123456789'
 str_lowercase= 'abcdefghijklmnopqrstuvwxyz'
 str_uppercase = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
-
+str_all = str_digits +  str_lowercase + str_uppercase
 
 def random_string():
-    #the numble of digits,lowercase and uppercase
-    digits_num = random.randint(1,3)
-    uppercase_num = random.randint(1,5-digits_num-1)
-    lowercase_num = 5 - (digits_num + uppercase_num)
-
-    #create a string
-    string = random.sample(str_digits,digits_num) + random.sample(str_uppercase,uppercase_num) + random.sample(str_lowercase,lowercase_num)
-
-    #shuffl
-    random.shuffle(string)
+    
+    string = random.sample(str_all,5)
 
     random_string = ''.join(string)
 
